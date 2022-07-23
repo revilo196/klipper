b2f8fb66056048f67e103010c7fe0b279f817d0f
Oliver <revilo196@users.noreply.github.com>
2022-07-23 02:58:24 +0200

    Update stm32f1.c

--- src/stm32/stm32f1.c
+++ src/stm32/stm32f1.c
@@ -256,7 +256,7 @@ usb_request_bootloader(void)
 
 /****************************************************************
  * Startup
- ****************************************************************/git merge --no-ff Klipper3d-master
+ ****************************************************************
     SCB->VTOR = (uint32_t)VectorTable;
 
     // Reset peripheral clocks (for some bootloaders that don't)
